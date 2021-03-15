d4c7310c9e562a7a0a9afe373034fe6f3f77e474
Nikogori <77167276+Nikogori@users.noreply.github.com>
2021-03-16 00:09:18 +0900

    Changed position of tracking codes

--- scripts/template.js
+++ scripts/template.js
@@ -1,13 +1,3 @@
-// Google Analytics
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
-  
-  ga('create', 'UA-111309875-3', 'auto')
-  ga('send', 'pageview')
-
 // insert header and footer template
 
 (function () {
@@ -120,3 +110,13 @@ function pgshow (e) {
 // pageshow fires after load and on Back/Forward
 
 window.addEventListener('pageshow', pgshow)
+
+// Google Analytics
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga')
+  
+  ga('create', 'UA-111309875-3', 'auto')
+  ga('send', 'pageview')
